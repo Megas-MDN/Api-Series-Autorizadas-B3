7d8095dec49f23478dfd45ac49604ffdf8b660ce
Megas-MDN <mdn.melqui@gmail.com>
2023-04-05 18:08:51 -0300

    fix: Mudando a lib puppeteer para a versao compativel na vercel

--- src/utils/getLinkB3.js
+++ src/utils/getLinkB3.js
@@ -1,7 +1,29 @@
-const puppeteer = require('puppeteer');
+require('dotenv/config');
+
+let puppeteer;
+let chrome = {};
+let opt = {};
+
+const setPuppeteer = async () => {
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    chrome = require('chrome-aws-lambda');
+    puppeteer = require('puppeteer-core');
+    opt = {
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+    console.log('puppeteer configurado');
+  } else {
+    puppeteer = require('puppeteer');
+  }
+};
 
 const main = async (url) => {
-  const browser = await puppeteer.launch();
+  await setPuppeteer();
+  const browser = await puppeteer.launch(opt);
   const page = await browser.newPage();
   await page.goto(url);
 
