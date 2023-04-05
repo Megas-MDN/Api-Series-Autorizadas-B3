c06323557748093301ddffff443f8b4099f3ec36
Megas-MDN <mdn.melqui@gmail.com>
2023-04-05 18:54:25 -0300

    chore: :wrench: Modificando as configuracoes do puppeteer para usar os parametros do dockerfile

--- src/utils/getLinkB3.js
+++ src/utils/getLinkB3.js
@@ -1,7 +1,19 @@
+require('dotenv/config');
 const puppeteer = require('puppeteer');
 
 const main = async (url) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--single-process',
+      '--no-zygote',
+    ],
+    executablePath:
+      process.env.NODE_ENV === 'production'
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
   await page.goto(url);
 
