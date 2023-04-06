698f894f3bb577f3e79cf14f879dc47a9072cbf9
Megas-MDN <mdn.melqui@gmail.com>
2023-04-05 21:35:28 -0300

    fix: :zap: Melhorando o sistema de path

--- src/utils/UnzipFile.js
+++ src/utils/UnzipFile.js
@@ -1,11 +1,13 @@
 const decompress = require('decompress');
 const path = require('path');
+const pathFileAssets = require('../../dirname');
 const fs = require('fs/promises');
 
 module.exports = async () => {
   try {
     console.log('Descompactando novo arquivo zip -> txt.');
-    await decompress('./src/assets/series.zip', './src/assets/', {
+    const pathFile = path.resolve(pathFileAssets, 'series.zip');
+    await decompress(pathFile, pathFileAssets, {
       map: (file) => {
         file.path = `series${path.extname(file.path)}`;
         return file;
--- src/utils/readWrite.js
+++ src/utils/readWrite.js
@@ -1,9 +1,15 @@
 const fs = require('fs/promises');
+const path = require('path');
+const pathFileAssets = require('../../dirname');
 
-const readFile = async (path) => fs.readFile(`./src/assets/${path}`, 'utf-8');
+const readFile = async (pathFile) =>
+  fs.readFile(path.resolve(pathFileAssets, pathFile), 'utf-8');
 
-const writeFile = async (file, path) => {
-  await fs.writeFile(`./src/assets/${path}`, JSON.stringify(file, null, 2));
+const writeFile = async (file, pathFile) => {
+  await fs.writeFile(
+    path.resolve(pathFileAssets, pathFile),
+    JSON.stringify(file, null, 2)
+  );
   console.log('Arquivo salvo com sucesso!');
 };
 
